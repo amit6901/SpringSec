95ca3ed55e0d9e4fba8f990ac875aa576976d088
amit6901 <amit.kandalkar06@gmail.com>
2024-08-16 23:40:33 +0530

    have explicitly give username and password

---
+++ SpringSec/src/main/java/com/project/SpringSec/HelloController.java
@@ -0,0 +1,15 @@
+package com.project.SpringSec;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome Home " + request.getSession().getId();
+    }
+
+}
