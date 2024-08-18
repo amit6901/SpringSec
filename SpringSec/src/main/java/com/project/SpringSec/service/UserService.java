8aecfafa489a23aee73bab67125df1454b8c8b73
amit6901 <amit.kandalkar06@gmail.com>
2024-08-19 00:10:26 +0530

    added authenticationManager to authenticate the users and pre for JWT token generation after authentication

--- SpringSec/src/main/java/com/project/SpringSec/service/UserService.java
+++ SpringSec/src/main/java/com/project/SpringSec/service/UserService.java
@@ -3,6 +3,9 @@ package com.project.SpringSec.service;
 import com.project.SpringSec.model.Users;
 import com.project.SpringSec.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +14,23 @@ public class UserService {
 
     @Autowired
     private UserRepo repo;
+    @Autowired
+    AuthenticationManager authManager;
+    @Autowired
+    private JWTService jwtService;
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
     public Users register(Users user){
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
     }
 
+    public String verify(Users user) {
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken();
+        else
+            return "fail";
+    }
 }
